b58dbc13c7a8ad3e4748e754a123de56ef0b48fe
Charlie Sharpsteen <source@sharpsteen.net>
2010-07-11 17:23:03 -0700

    Added compass styling to the project

--- tryRforStats.rb
+++ tryRforStats.rb
@@ -2,6 +2,20 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+  end
+    
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
 
 get '/' do
   redirect '/index.html'
