51defb99d15e99397b974668d22b9b185a09cd74
Charlie Sharpsteen <source@sharpsteen.net>
2010-07-11 20:59:44 -0700

    Worked on connection the R terminal to the WebSocket. Problems occurred.
    Basically, something screwy is going on- the responses seem to lag the
    commands by 1 evaluation.  I.e.:
    
      > 1
    
    Returns nothing.  Then:
    
      > 2+2
    
    Returns:
    
      "[1] 1"
    
    Which is the result of the first command, not the second.  Could be a
    bug in the way I implemented the rServe controller or it could be due to
    the fact that the WebSocket communication is a non-blocking call and
    things are not synching up.

--- static/js/rforstats.js
+++ static/js/rforstats.js
@@ -2,7 +2,50 @@
 
   var rConsole; // Globally accessable R console controller.
 
-  $(document).ready(function(){
+	var rServe = {
+	
+		connect: function(){
+			this._ws=new WebSocket("ws://localhost:8002");
+			this._ws.onopen=this._onopen;
+			this._ws.onmessage=this._onmessage;
+			this._ws.onclose=this._onclose;
+		},
+
+		send: function(message){
+			if (this._ws )
+			this._ws.send(message);
+		},
+	
+		_onopen: function(){
+		},	
+	
+		_onmessage: function(m) {
+			if (m.data){
+				rServe.response = m.data; 
+      }else{
+        rServe.response = null;
+      }
+		},
+			
+		_onclose: function(m) {
+			this._ws=null;	
+		},
+		
+		response: null
+		
+	};
+
+	function pausecomp(millis)
+	 {
+	  var date = new Date();
+	  var curDate = null;
+	  do { curDate = new Date(); }
+	  while(curDate-date < millis);
+	}
+
+  $(document).ready(function(){  
+    
+    rServe.connect();
     
     var console = $('.console');
     rConsole = console.console({
@@ -18,15 +61,22 @@
       
       // Callbacks
       commandValidate: function(line){
-	      if (line == "") return false; // Empty line is invalid
+	      if (line == "" || line == null) return false; // Empty line is invalid
 	      else return true;
       },
       
       commandHandle: function(line,report){
-        report(line);
-      }
+          rServe.send( line );
+          pausecomp(2000);
+          this.commandRetrieve(report)
+      },
       
-      
+      commandRetrieve: function( report ){      
+        if( this.commandValidate( rServe.response) ) report( rServe.response );
+        else report('');
+      }    
     });
+    
+    
   });
 })(jQuery);   // End jQuery wrapper.
