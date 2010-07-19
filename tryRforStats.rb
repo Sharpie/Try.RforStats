0edaa8e4e7b47a0fc692fc5ad2bf9461e57e9738
Charlie Sharpsteen <source@sharpsteen.net>
2010-07-19 10:52:26 -0700

    Removed compass configuration from Sinatra
    No point when Compass is compiling to static stylesheets.

--- tryRforStats.rb
+++ tryRforStats.rb
@@ -10,13 +10,7 @@ require 'haml'
 set:public, File.dirname(__FILE__) + '/static'
 
 configure do
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-  end
-    
   set :haml, { :format => :html5 }
-  set :sass, Compass.sass_engine_options
 end
 
 
